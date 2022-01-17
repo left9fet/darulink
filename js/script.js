7555d7abb1e3cbc3d7b0001933ff9b7ef7fcbc63
Igor Hudakov <left9fet@gmail.com>
2022-01-17 22:43:13 +0500

    add fixed header on mobile devices

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ header = document.querySelector('.header__top');
 menu.onclick = function () {
     header.classList.toggle('header__top--active');
 };
+
+
+let waypoint = new Waypoint({
+    element: document.getElementById('point'),
+
+    handler: function (direction) {
+        if (direction = 'down') {
+            header.classList.toggle('header__top--scroll');
+        }
+    }
+})
